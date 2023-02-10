013373cdd38bd1c6625053630e7bce1204a75ee9
OscarUrielCamposViento <124831938+OscarUrielCamposViento@users.noreply.github.com>
2023-02-10 14:40:04 -0600

    Add files via upload

---
+++ tutoriales basicos de lit/Build a word viewer!/3 Passing in properties with attributes!/word-viewer.ts
@@ -0,0 +1,11 @@
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('word-viewer')
+class WordViewer extends LitElement {
+  @property() words = 'initial value';
+
+  render() {
+    return html`<pre>${this.words}</pre>`
+  }
+}
